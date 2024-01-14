353f9ce4f251fe276d79483b285921ec8f6c321a
mahi000007 <mahendran1482004@gmail.com>
2024-01-14 20:37:50 +0530

    New Solution Added For 3 Sum

---
+++ 3Sum.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1;
+            int right = n - 1;
+            int sum = 0;
+            while (left < right)
+            {
+                sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0)
+                {
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                }
+            }
+        }
+        return ans;
+    }
+};
