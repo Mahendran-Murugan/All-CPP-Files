379cc826143deb954be1176b7ab6607356534d8e
mahi000007 <mahendran1482004@gmail.com>
2024-01-17 16:59:03 +0530

    feat: Dynamic Programming Memoisation added

--- Dynamic Programming/memoisation for Fibonacci.cpp
+++ Dynamic Programming/memoisation for Fibonacci.cpp
@@ -1,9 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 // Normal way of Using Recursion to Calculate Fibonacci
 
 /*
+#include <bits/stdc++.h>
+using namespace std;
+
     int fibo(int num)
 {
     if (num <= 1)
@@ -19,3 +19,28 @@ int main()
     return 0;
 }
 */
+
+// Using Memoisation it reduces the time complexity
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(vector<int> mem, int num)
+{
+    if (num <= 1)
+        return num;
+
+    if (mem[num] != -1)
+        return mem[num];
+
+    return mem[num] = fibo(mem, num - 1) + fibo(mem, num - 2);
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    vector<int> mem(num + 1, -1);
+    cout << fibo(mem, num);
+    return 0;
+}
