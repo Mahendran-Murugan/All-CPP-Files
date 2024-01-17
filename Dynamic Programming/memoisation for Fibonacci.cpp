962c98deb0b4019790032d944b728abbe3903771
mahi000007 <mahendran1482004@gmail.com>
2024-01-17 16:51:48 +0530

    Normal Recursion for Fibonacci is Calculated

---
+++ Dynamic Programming/memoisation for Fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Normal way of Using Recursion to Calculate Fibonacci
+
+/*
+    int fibo(int num)
+{
+    if (num <= 1)
+        return num;
+    return fibo(num - 1) + fibo(num - 2);
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << fibo(num);
+    return 0;
+}
+*/
