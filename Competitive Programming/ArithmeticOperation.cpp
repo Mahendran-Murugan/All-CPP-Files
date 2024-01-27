965ca852e7b56b50b9b88f41f05aff4933b10cd7
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-01-27 14:08:10 +0530

    feat: ArithmeticOperation soln

---
+++ Competitive Programming/ArithmeticOperation.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a = 0, b = 0;
+    cin >> a >> b;
+    cout << "Addition: " << a+b << endl;
+    cout << "Subtraction: " << a-b << endl;
+    cout << "Multiplication: " << a*b << endl;
+    cout << "Division: " << a/b << endl;
+    return 0;
+}
