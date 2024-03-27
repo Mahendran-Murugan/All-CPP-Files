4eebf0c4372488e16b5ac011fdd0b7c75e6e1c77
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-27 21:32:56 +0530

    feat: add code"

--- Competitive Programming/BinarySubArraysWithSum.cpp
+++ Competitive Programming/BinarySubArraysWithSum.cpp
@@ -1,14 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
         unordered_map<int, int> mpp;
         mpp[0] = 1;
         int curr = 0;
         int ans = 0;
 
-        for (int num : nums) {
+        for (int num : nums)
+        {
             curr += num;
-            if (mpp.find(curr - goal) != mpp.end()) {
+            if (mpp.find(curr - goal) != mpp.end())
+            {
                 ans += mpp[curr - goal];
             }
             mpp[curr]++;
